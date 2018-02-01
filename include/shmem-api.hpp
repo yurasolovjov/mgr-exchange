e4d43707e2f5a51b106e2acf137e8cd7f673a873
solovjevui <yura.solovjov@mail.ru>
2018-02-01 16:32:01 +0300

    fix

---
+++ include/shmem-api.hpp
@@ -0,0 +1,140 @@
+#ifndef SHMEM_API_H
+#define SHMEM_API_H
+
+#include <iostream>
+#include <cstring>
+#include <argparser.h>
+#include <signal.h>
+#include <memory>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/program_options.hpp>
+#include <memory>
+#include "signals/shsignals.h"
+
+
+namespace ShSignals {
+
+    static const uint32_t default_size_shmem = 1024;
+
+    /** Пространство для работы с РП и обработкой входного массива данных */
+    using namespace boost::interprocess;
+    namespace po = boost::program_options;
+
+
+    /** Создание разделяемой памяти*/
+    template<typename T>
+    std::unique_ptr<managed_shared_memory> created(std::string shmem, uint32_t size){
+
+        if( !shmem.empty() && size > 0){
+
+            try{
+                return std::make_unique<managed_shared_memory>(open_or_create,shmem.c_str(),size);
+            }
+            catch(interprocess_exception& ex){
+                return nullptr;
+            }
+        }
+        else return nullptr;
+    }
+
+    /** Подключение к разделяемой памяти*/
+    std::unique_ptr<managed_shared_memory> connect(std::string shmem){
+
+        if( !shmem.empty() ){
+
+            try{
+                return std::make_unique<managed_shared_memory>(open_or_create, shmem.c_str(), ShSignals::default_size_shmem );
+            }
+            catch(interprocess_exception& ex){
+                return nullptr;
+            }
+        }
+        else return nullptr;
+
+    }
+
+    /** @brief шаблонная функция чтения
+     *  @param name - имя сигнала разделяемой памяти
+     */
+    template<typename T>
+    bool read(std::string  signal, managed_shared_memory* segment){
+
+        if(signal.empty()){
+            std::cout<<" Signal name is empty"<<std::endl;
+            return false;
+        }
+
+        std::pair<T*, managed_shared_memory::size_type> res;
+
+        res = segment->find<T>(signal.c_str());
+
+        if(res.second != 1){
+            std::cerr<<"*** SIGNAL: "<<signal<<" *** not found"<<std::endl;
+            return false;
+        }
+
+        if(std::is_same<T,ShSignals::SignalPairInt>::value){
+            std::cout<<"Signal "<<signal<<" value: "<<res.first->first<<" | "<<res.first->second<<std::endl;
+        }
+        else if( std::is_same<T,ShSignals::Analog>::value){
+        }
+        else if( std::is_same<T,ShSignals::Discret>::value){
+        }
+        else if( std::is_same<T,ShSignals::Hardware>::value){
+        }
+        else if( std::is_same<T,ShSignals::Software>::value){
+        }
+        else{
+        }
+    }
+
+    /** @brief шаблонная функция записи
+     *  @param name - имя сигнала разделяемой памяти
+     *  @param value - строка содержащая значения signal
+     */
+    template<typename T, typename ...Args>
+    bool write(std::string  signal, managed_shared_memory* segment, Args& ...value){
+
+        if(signal.empty()){
+            std::cout<<" Signal name is empty"<<std::endl;
+            return false;
+        }
+
+        //std::pair<T*, managed_shared_memory::size_type> res;
+
+//        res = segment->find<T>(signal.c_str());
+
+        T* obj = segment->find_or_construct<T>(signal.c_str())(value...);
+
+
+        if(std::is_same<T,ShSignals::SignalPairInt>::value){
+        }
+        else if( std::is_same<T,ShSignals::Analog>::value){
+        }
+        else if( std::is_same<T,ShSignals::Discret>::value){
+        }
+        else if( std::is_same<T,ShSignals::Hardware>::value){
+        }
+        else if( std::is_same<T,ShSignals::Software>::value){
+        }
+        else{
+        }
+    }
+
+    /** @brief шаблонная функция обработки сигнала
+     *  @param name - имя сигнала разделяемой памяти
+     *  @param value - строка содержащая значения signal
+     */
+    template<typename T>
+    bool process(std::string  signal, std::string value, managed_shared_memory& segment);
+
+    /** @brief шаблонная функция обработки сигнала
+     *  @param name - имя сигнала разделяемой памяти
+     *  @param segment - управляющая структура памяти
+     */
+    template<typename T>
+    bool remove(std::string  signal, managed_shared_memory& segment);
+
+
+}
+#endif // SHMEM_API_H
--- utility/main.cpp
+++ utility/main.cpp
@@ -1,7 +1,15 @@
-#include "shsignals.h"
-#include "main.h"
+#include <shmem-api.hpp>
 
+#include <iostream>
+#include <cstring>
+#include <argparser.h>
+#include <signal.h>
+#include <memory>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/program_options.hpp>
 
+using namespace boost::interprocess;
+namespace po = boost::program_options;
 int main(int argc, char** argv)
 {
 
@@ -81,42 +89,42 @@ int main(int argc, char** argv)
 
     try{
 
-        managed_shared_memory segment(open_only, shmem.c_str());
+        std::unique_ptr<managed_shared_memory> segment = ShSignals::connect(shmem);
 
         /** @brief Чтение сигнала из разделяемой памяти */
         if(!signal.empty() && operationRead){
 
-            read<ShSignals::SignalPairInt>(signal, segment);
+            ShSignals::read<ShSignals::SignalPairInt>(signal, segment.get());
         }
 
         /** @brief Запись сигнала в разделяемую память */
         if(!signal.empty() && operationWrite){
-            std::pair<ShSignals::SignalPairInt*, managed_shared_memory::size_type> res;
-            res = segment.find<ShSignals::SignalPairInt>(signal.c_str());
 
-            if( res.second == 1){
-                res.first->first = 0.0;
-                res.first->second = 0;
-                std::cout<<" *** SIGNAL *** "<<signal<<" : "<<res.first->first<<" "<<res.first->second<<std::endl;
-            }
+            ShSignals::SignalPairInt a(100,100);
+            ShSignals::write<ShSignals::SignalPairInt>(signal,segment.get(), a);
+
         }
 
         /* Список имён в РП*/
         if(operationList){
+            /*
             for(auto itr = segment.named_begin(); itr != segment.named_end(); ++itr){
                 static int i = 0;
                 const managed_shared_memory::char_type *name = itr->name();
                 std::cout<<"Signal "<<i<<" : "<<name<<std::endl;
                 i++;
             }
+            */
         }
 
         if(!destroy.empty()){
-            bool r = segment.destroy<ShSignals::SignalPairInt>(destroy.c_str());
+//            bool r = segment.destroy<ShSignals::SignalPairInt>(destroy.c_str());
 
+            /*
             if( r ){
                 std::cout<<"*** SIGNAL: "<<destroy<<" has been destroyed"<<std::endl;
             }
+            */
         }
     }
     catch(interprocess_exception& ex){
@@ -126,38 +134,4 @@ int main(int argc, char** argv)
 
 
     return 0;
-}
-
-template<typename T>
-bool read(std::string  signal, managed_shared_memory& segment){
-
-    if(signal.empty()){
-        std::cout<<" Signal name is empty"<<std::endl;
-        return false;
-    }
-
-    std::pair<T*, managed_shared_memory::size_type> res;
-
-    res = segment.find<T>(signal.c_str());
-
-    if(res.second != 1){
-        std::cerr<<"*** SIGNAL: "<<signal<<" *** not found"<<std::endl;
-        return false;
-    }
-
-    if(std::is_same<T,ShSignals::SignalPairInt>::value){
-
-        std::cout<<" *** SIGNAL *** "<<signal<<" : "<<res.first->first<<" "<<res.first->second<<std::endl;
-    }
-    else if( std::is_same<T,ShSignals::Analog>::value){
-    }
-    else if( std::is_same<T,ShSignals::Discret>::value){
-    }
-    else if( std::is_same<T,ShSignals::Hardware>::value){
-    }
-    else if( std::is_same<T,ShSignals::Software>::value){
-    }
-    else{
-    }
-
 }
